52b5c1fb2f89877808096b822fd5d80eda3378a4
unknown <jnocera3@gmail.com>
2025-02-14 09:19:57 -0500

    Initial Commit

---
+++ br_splits.py
@@ -0,0 +1,160 @@
+#!/usr/bin/python
+import requests
+import argparse
+import pandas as pd
+import csv
+import sys
+import time
+from pybaseball.split_stats import get_splits
+from bs4 import BeautifulSoup
+from urllib.parse import *
+
+def find_url(url, playername):
+    try:
+        response = requests.get(url)
+    except:
+        print(f"Request failed {url}")
+        return
+#   print(response.request.headers)
+#   print(response.text)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+#       print (soup)
+        print ("Searching for " + playername)
+#       playerurl = soup.find('a', string=playername).attrs["href"]
+        players_active = soup.find_all('b')
+        for player in players_active:
+            playerurl = player.find('a', string=playername)
+            if playerurl:
+                print("Found: " + str(playerurl["href"]))
+                return url + "/" + playerurl["href"].split('/')[3]
+#       No player found. Return input url
+        return "Missing"
+
+# Argument Parsing
+parser = argparse.ArgumentParser()
+
+# Read in arguments
+parser.add_argument("-url","--url", required=False, default="https://www.baseball-reference.com", help='URL to scrape')
+parser.add_argument("-year","--year", type=int, required=True, help='Year to pull stats for')
+parser.add_argument("-playerlist","--playerlist", required=True, help='File containing list of players')
+parser.add_argument("-outfile","--outfile", required=True, help='Name of file to output player stats to')
+
+# Parse the input
+args = parser.parse_args()
+
+# Store the arguments
+url = str(args.url)
+year = args.year
+playerlist_file = str(args.playerlist)
+out_file = str(args.outfile)
+
+# Define search string for player totals
+player_totals = str(year) + " Totals"
+
+# Open output file
+f = open(out_file, "w")
+
+# Read player list file
+with open(playerlist_file, encoding='utf-8') as csvfile:
+
+    # Skips the headersg
+    # Using next() method
+    next(csvfile)
+    next(csvfile)
+
+    # Create reader object by passing the file
+    # object to reader method
+    reader_obj = csv.reader(csvfile)
+
+    # Iterate over each row in the csv file
+    # using reader object
+    for row in reader_obj:
+        player_type = row[2].strip().upper()
+        first_letter = row[0][0:1].lower()
+        player_name = row[1].strip() + " " + row[0].strip()
+        # Construct URL to search for player page
+        letter_url = url + "/players/" + first_letter
+#       print(player_type + " " + first_letter + " " + player_name + " " + letter_url)
+        # Find player url
+        player_url = find_url(letter_url,player_name)
+#       print (player_url)
+        if player_url != "Missing":
+            # Extract out player id
+            player_id = player_url.split('/')[-1].split('.')[0]
+            print ("Player ID: " + str(player_id))
+            # Create header with link for html table
+            html_header = '<h2><a href="' + player_url + '" target="_blank">' + player_name + "</a></h2>\n"
+            # Store splits in a dataframe
+            if player_type == "P":
+                df = get_splits(player_id, year=year, pitching_splits=True)[0]
+                df_pitch = get_splits(player_id, year=year, pitching_splits=True)[1]
+            else: 
+                df = get_splits(player_id, year=year)
+        else:
+            print (player_name + " not found at " + url + ". Check to make sure player name is correct.")
+            sys.exit(1)
+        # Attempt to extract out season totals
+        if player_totals in df.loc['Season Totals'].index:
+            totals = pd.Series(df.loc['Season Totals'].loc[player_totals],name=player_totals)
+            # Extract out platoon splits
+            if player_type == "P":
+                platoon = df.loc['Platoon Splits'].loc[['vs RHB','vs LHB']]
+                totals_pitch = pd.Series(df_pitch.loc['Season Totals -- Game-Level'].loc[player_totals],name=player_totals)
+                totals = pd.concat([totals,totals_pitch[["GS","ERA"]]])
+                
+                platoon["GS"] = [totals_pitch["GS"], totals_pitch["GS"]]
+                platoon = platoon.assign(GS=[totals_pitch["GS"], totals_pitch["GS"]], ERA=[totals_pitch["ERA"], totals_pitch["ERA"]])
+            else: 
+                positions = df.loc['Defensive Positions'][df.loc["Defensive Positions"]["G"] > 1].index.str.replace("as ","").to_list()
+                if "PH for DH" in positions:
+                    positions.remove("PH for DH")
+                if "PH" in positions:
+                    positions.remove("PH")
+                positions = ','.join(positions)
+                platoon = df.loc['Platoon Splits'].loc[['vs RHP','vs LHP']]
+            # Store needed data in new dataframe
+            splits = pd.DataFrame()
+            #splits = splits.append(totals)
+            splits = pd.concat([splits,totals.to_frame().T])
+            # Merge stats into final dataframe
+            splits = pd.concat([splits, platoon], ignore_index=False)
+            # Convert some stats to integers
+            splits[['BA', 'OBP', 'SLG', 'OPS']] = splits[['BA', 'OBP', 'SLG', 'OPS']] * 1000.0
+            # Remove unneeded stats
+            if player_type == "P":
+                splits.drop(['PA', 'SO/W', 'TB', 'SH', 'SF', 'IBB', 'ROE', 'BAbip', 'tOPS+', 'sOPS+', '1B'], axis=1, inplace=True) 
+                # Convert ERA to an integer
+                splits['ERA'] = splits['ERA'] * 100.0
+            else:
+                splits.drop(['GS', 'PA', 'TB', 'SH', 'SF', 'IBB', 'ROE', 'BAbip', 'tOPS+', 'sOPS+', '1B'], axis=1, inplace=True) 
+            # Convert entire dataframe to integers
+            splits = splits.astype(int)
+            # Convert ERA back to float
+            if player_type == "P":
+                splits['ERA'] = (splits['ERA'].astype(float) * 0.01).round(2).astype(str)
+                # Rearrange columns
+                cols = splits.columns.to_list()
+                cols = [cols[0]] + cols[17:] + cols[1:17]
+                splits = splits[cols]
+            else:
+                # Add positions for batters
+                splits = splits.assign(Pos=[positions, "N/A", "N/A"])
+            # Convert dataframe to html table
+            styled_splits = splits.style.set_table_styles([
+                {'selector': 'th', 'props': [('text-align', 'right'), ('border', 'none')]},
+                {'selector': 'td', 'props': [('text-align', 'right'), ('border', 'none'), ('width', '50')]}
+            ])
+            html_table = styled_splits.to_html()
+        else:
+            # Add message to html file that no stats are available.
+            html_table = '<b>Did not play MLB in ' + str(year) + '.</b>'
+        # Add header to html table
+        html_table = html_header + html_table
+        # Write out data to output file
+        f.write(html_table)
+        # Sleep 10 seconds between requests to avoid being blocked
+        time.sleep(10)
+
+# Close output file
+f.close()
---
+++ game_stats.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python
+import argparse
+import pandas as pd
+import csv
+import sys
+import time
+import os
+import datetime
+from pybaseball.league_batting_stats import batting_stats_range
+from pybaseball.league_pitching_stats import pitching_stats_range
+
+# Get current working directory
+cwd = os.getcwd()
+
+# Argument Parsing
+parser = argparse.ArgumentParser()
+
+# Read in arguments
+parser.add_argument("-playerlist","--playerlist", required=True, help='File containing list of players')
+parser.add_argument("-date","--date", required=False, default=datetime.date.today()-datetime.timedelta(days=1), help='Date to pull stats for')
+
+# Parse the input
+args = parser.parse_args()
+
+# Store the date as datetime object
+date = datetime.datetime.strptime(str(args.date), "%Y-%m-%d").date()
+# Find previous and next dates
+previous_date = str(date - datetime.timedelta(days=1))
+next_date = str(date + datetime.timedelta(days=1))
+# Set current date to type string
+date = str(date)
+
+# Store list of players
+playerlist_file = str(args.playerlist)
+
+# Define output file and links to files before and after current date
+out_file = "stats_" + date + ".html"
+previous_file = "file://" + cwd + "/stats_" + previous_date + ".html"
+next_file = "file://" + cwd + "/stats_" + next_date + ".html"
+
+# Get all pitching stats for input date
+try:
+    all_pitchers = pitching_stats_range(date)
+    all_pitchers.drop(['Age', '#days', 'Lev', 'Date', 'Tm', 'Opp', 'W', 'L', 'ERA', 'GSc', 'AB', 'IBB', 'SF', 'Str', 'StL', 'StS', 'GB/FB', 'LD', 'PU', 'WHIP', 'BAbip', 'SO9', 'SO/W', 'mlbID'], axis=1, inplace=True) 
+    all_pitchers.drop(all_pitchers.columns[1], axis=1, inplace=True)
+    all_pitchers["SV"] = all_pitchers["SV"].fillna(0).astype(int)
+except:
+    all_pitchers = pd.DataFrame()
+
+# Get all batting stats for input date
+try: 
+    all_batters = batting_stats_range(date)
+    all_batters.drop(['Age', '#days', 'Lev', 'Date', 'Tm', 'Opp', 'G', 'PA', 'BA', 'OBP', 'SLG', 'OPS', 'mlbID'], axis=1, inplace=True) 
+    all_batters.drop(all_batters.columns[1], axis=1, inplace=True)
+except:
+    all_batters = pd.DataFrame()
+
+# Define dataframes for storing batter and pitcher stats
+batters = pd.DataFrame()
+pitchers = pd.DataFrame()
+
+# Loop over player list
+with open(playerlist_file) as csvfile:
+
+    # Skips the headersg
+    # Using next() method
+    next(csvfile)
+    next(csvfile)
+
+    # Create reader object by passing the file
+    # object to reader method
+    reader_obj = csv.reader(csvfile)
+
+    # Iterate over each row in the csv file
+    # using reader object
+    for row in reader_obj:
+        player_type = row[2].strip().upper()
+        player_name = row[1].strip() + " " + row[0].strip()
+#       print(player_type + " " + first_letter + " " + player_name + " " + letter_url)
+#       print (player_url)
+#       Check to see if player played
+        if not all_batters.empty and player_type == "B" and all_batters['Name'].isin([player_name]).any():
+#           print(all_batters[all_batters['Name'] == player_name])
+            batters = pd.concat([batters,all_batters[all_batters['Name'] == player_name]])
+        if not all_pitchers.empty and player_type == "P" and all_pitchers['Name'].isin([player_name]).any():
+#           print(all_pitchers[all_pitchers['Name'] == player_name])
+            pitchers = pd.concat([pitchers,all_pitchers[all_pitchers['Name'] == player_name]])
+
+# If dataframe is populated set player name to dataframe index
+if not batters.empty:
+    batters.set_index('Name', inplace=True)
+    batters.index.name = None
+    print(batters)
+    # Write out styled data to html table for batters
+    styled_batters = batters.style.set_table_styles([
+        {'selector': 'th', 'props': [('text-align', 'right'), ('border', 'none')]},
+        {'selector': 'td', 'props': [('text-align', 'right'), ('border', 'none'), ('width', '50')]}
+    ])
+    html_batters_table = styled_batters.to_html()
+else:
+    html_batters_table = '<p>No batters hit on this day</p>\n'
+if not pitchers.empty:
+    pitchers.set_index('Name', inplace=True)
+    pitchers.index.name = None
+    #Set IP to 1 decimal place and ERA to 2 decimal places
+    pitchers['IP'] = pitchers['IP'].astype(str)
+    #pitchers['ERA'] = pitchers['ERA'].apply(lambda x: '{:,.2f}'.format(x))
+    print(pitchers)
+    # Write out styled data to html table for pitchers
+    styled_pitchers = pitchers.style.set_table_styles([
+        {'selector': 'th', 'props': [('text-align', 'right'), ('border', 'none')]},
+        {'selector': 'td', 'props': [('text-align', 'right'), ('border', 'none'), ('width', '50')]}
+    ])
+    html_pitchers_table = styled_pitchers.to_html()
+else:
+    html_pitchers_table = '<p>No pitchers pitched on this day</p>\n'
+
+# Create header for html file
+html_header = '<center><a href="' + previous_file + '">Previous</a>&nbsp;<a href="' + next_file + '">Next</a>\n<h2>Stats for ' + date + '</h2></center>\n<b style="margin-left:10px">Batters</b>\n' 
+# Add header to html batters table
+html_table = html_header + html_batters_table
+# Add header for pitchers
+html_table = html_table + '<br><br><b style="margin-left:10px">Pitchers</b>\n'
+# Add html pitchers table
+html_table = html_table + html_pitchers_table
+
+# Open output file
+f = open(out_file, "w")
+# Read player list file
+# Write out data to output file
+f.write(html_table)
+# Close output file
+f.close()
